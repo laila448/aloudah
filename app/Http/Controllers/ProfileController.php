189da63ca2c65c2b3ad77345d1189ca8fef592e2
yumnaqass <yumnaqassomah@email.com>
2024-06-22 19:30:35 +0300

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,6 +10,7 @@ use App\Models\Vacation;
 use App\Models\Warehouse_Manager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -74,12 +75,30 @@ class ProfileController extends Controller
 
     public function editMyProfile(Request $request){
 
+        $validator = Validator::make($request->all(),[
+                'email' => 'string|email',
+                'phone_number' => 'min:4|max:15',
+                'address' => 'string'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->toJson()
+            ], 400);
+        }
         try{
+
             if(Auth::guard('admin')->check()){
                 $user_id = Auth::guard('admin')->id();
-                $admin = Admin::where('id' , $user_id)->update([
-                    'phone_number' => $request->phone_number
-                ]);
+                $admin = Admin::where('id' , $user_id)->first();
+                if($request->exists('phone_number')){
+                    $admin->phone_number= $request->phone_number ;
+                }if($request->exists('email')){
+                    $admin->email= $request->email ;
+                }
+
+                $admin->save();
+
                 return response()->json([
                     'success' => true,
                     'message' => 'Admin profile updated successfully.'
@@ -87,10 +106,16 @@ class ProfileController extends Controller
             }
             elseif(Auth::guard('employee')->check()){
                 $user_id = Auth::guard('employee')->id();
-                $employee = Employee::where('id' , $user_id)->update([
-                    'phone_number' => $request->phone_number,
-                    'address' => $request->address
-                ]);
+                $employee = Employee::where('id' , $user_id)->first();
+                if($request->exists('phone_number')){
+                    $employee->phone_number= $request->phone_number ;
+                }if($request->exists('email')){
+                    $employee->email= $request->email ;
+                }if($request->exists('address')){
+                    $employee->address= $request->address ;
+                }
+                $employee->save();
+
                  return response()->json([
                      'success' => true,
                      'message' => 'employee profile updated successfully.'
@@ -98,10 +123,16 @@ class ProfileController extends Controller
             }
             elseif(Auth::guard('branch_manager')->check()){
                 $user_id = Auth::guard('branch_manager')->id();
-                $manager = Branch_Manager::where('id' , $user_id)->update([
-                    'phone_number' => $request->phone_number,
-                    'manager_address' => $request->address
-                ]);
+                $manager = Branch_Manager::where('id' , $user_id)->first();
+                if($request->exists('phone_number')){
+                    $manager->phone_number= $request->phone_number ;
+                }if($request->exists('email')){
+                    $manager->email= $request->email ;
+                }if($request->exists('address')){
+                    $manager->manager_address= $request->address ;
+                }
+                $manager->save();
+               
                  return response()->json([
                      'success' => true,
                      'message' => 'Barnch manager profile updated successfully.'
@@ -109,10 +140,16 @@ class ProfileController extends Controller
             }
             elseif(Auth::guard('warehouse_manager')->check()){
                 $user_id = Auth::guard('warehouse_manager')->id();
-                $manager = Warehouse_Manager::where('id' , $user_id)->update([
-                    'phone_number' => $request->phone_number,
-                    'manager_address' => $request->address
-                ]);
+                $manager = Warehouse_Manager::where('id' , $user_id)->first();
+                if($request->exists('phone_number')){
+                    $manager->phone_number= $request->phone_number ;
+                }if($request->exists('email')){
+                    $manager->email= $request->email ;
+                }if($request->exists('address')){
+                    $manager->manager_address= $request->address ;
+                }
+                $manager->save();
+               
                  return response()->json([
                      'success' => true,
                      'message' => 'Warehouse manager profile updated successfully.'
